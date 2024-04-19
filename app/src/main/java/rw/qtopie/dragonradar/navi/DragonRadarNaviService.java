3385528a26f3e27bd3286ea17bfd3f100b0c962b
qtopie.rw <qtopie.rw@gmail.com>
2024-04-19 09:20:49 +0800

    fix notification

--- app/src/main/java/rw/qtopie/dragonradar/navi/DragonRadarNaviService.java
+++ app/src/main/java/rw/qtopie/dragonradar/navi/DragonRadarNaviService.java
@@ -5,6 +5,7 @@ import static rw.qtopie.dragonradar.navi.DragonRouteActivity.DEBUG_TAG;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
@@ -13,6 +14,8 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import androidx.wear.ongoing.OngoingActivity;
+import androidx.wear.ongoing.Status;
 
 import com.amap.api.maps.AMapException;
 import com.amap.api.navi.AMapNavi;
@@ -32,9 +35,14 @@ import com.amap.api.navi.model.AimLessModeCongestionInfo;
 import com.amap.api.navi.model.AimLessModeStat;
 import com.amap.api.navi.model.NaviInfo;
 
+import rw.qtopie.dragonradar.MainActivity;
+import rw.qtopie.dragonradar.R;
+
 public class DragonRadarNaviService extends Service implements AMapNaviListener, ParallelRoadListener {
 
-    public static final String NAVIGATION_SERVICE_NOTIFICATION_ID = "navigation_channel_id";
+    public static final String NOTIFICATION_CHANNEL_ID = "riding_navi_channel_01";
+    private static final int NOTIFICATION_ID = 330704;
+
     private AMapNavi mAMapNavi;
 
     @Override
@@ -62,23 +70,83 @@ public class DragonRadarNaviService extends Service implements AMapNaviListener,
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // The service is starting, due to a call to startService()
-       startForeground();
-       return Service.START_STICKY_COMPATIBILITY;
+        startForeground();
+        Log.d("dragonradar", "started service");
+        return Service.START_STICKY_COMPATIBILITY;
     }
 
     private void startForeground() {
-        NotificationChannel channel = new NotificationChannel(NAVIGATION_SERVICE_NOTIFICATION_ID, "Dragon Radar is running", NotificationManager.IMPORTANCE_DEFAULT);
-        channel.setDescription("Dragon Radar is running for navigating");
+        Log.d("dragonradar", "starting service");
+        String titleText = getString(R.string.app_name);
+        String text = getString(R.string.app_name);
 
-        NotificationManager nm = (NotificationManager) getSystemService(NotificationManager.class);
+        NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, text, NotificationManager.IMPORTANCE_DEFAULT);
+
+        NotificationManager nm = getSystemService(NotificationManager.class);
         nm.createNotificationChannel(channel);
 
-        Notification notification =
-                new NotificationCompat.Builder(this.getApplicationContext(), NAVIGATION_SERVICE_NOTIFICATION_ID)
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle().bigText("test").setBigContentTitle("test123");
+        Intent launchActivityIntent = new Intent(this.getApplicationContext(), DragonRouteActivity.class);
+
+        PendingIntent activityPendingIntent = PendingIntent.getActivity(
+                this.getApplicationContext(),
+                0,
+                launchActivityIntent,
+                PendingIntent.FLAG_IMMUTABLE
+        );
+
+//        val cancelIntent = Intent(this, ForegroundOnlyWalkingWorkoutService::class.java)
+//        cancelIntent.putExtra(EXTRA_CANCEL_WORKOUT_FROM_NOTIFICATION, true)
+//
+//        val servicePendingIntent = PendingIntent.getService(
+//                this,
+//                0,
+//                cancelIntent,
+//                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+//                )
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this.getApplicationContext(), NOTIFICATION_CHANNEL_ID)
+                        .setStyle(bigTextStyle)
+                        .setContentTitle(titleText)
+                        .setContentText(titleText)
+                        .setSmallIcon(R.drawable.ic_walk)
+                        .setDefaults(NotificationCompat.DEFAULT_ALL)
                         .setOngoing(true)
+                        .addAction(new NotificationCompat.Action(R.drawable.ic_walk, getString(R.string.app_name),
+                                activityPendingIntent))
+                        .setCategory(NotificationCompat.CATEGORY_WORKOUT)
+                        .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                        .setOngoing(true);
+
+        Status ongoingActivityStatus = new Status.Builder()
+                // Sets the text used across various surfaces.
+                .addTemplate(text)
+                .build();
+
+        OngoingActivity ongoingActivity =
+                new OngoingActivity.Builder(this.getApplicationContext(), NOTIFICATION_ID, notificationBuilder)
+                        // Sets icon that will appear on the watch face in active mode. If it isn't set,
+                        // the watch face will use the static icon in active mode.
+                        .setAnimatedIcon(R.drawable.animated_walk)
+                        // Sets the icon that will appear on the watch face in ambient mode.
+                        // Falls back to Notification's smallIcon if not set. If neither is set,
+                        // an Exception is thrown.
+                        .setStaticIcon(R.drawable.ic_walk)
+                        // Sets the tap/touch event, so users can re-enter your app from the
+                        // other surfaces.
+                        // Falls back to Notification's contentIntent if not set. If neither is set,
+                        // an Exception is thrown.
+                        .setTouchIntent(activityPendingIntent)
+                        // In our case, sets the text used for the Ongoing Activity (more options are
+                        // available for timers and stop watches).
+                        .setStatus(ongoingActivityStatus)
                         .build();
+
+        ongoingActivity.apply(this.getApplicationContext());
+
         int type = ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION;
-        startForeground(100, notification, type);
+        startForeground(NOTIFICATION_ID, notificationBuilder.build(), type);
     }
 
 
--- app/src/main/java/rw/qtopie/dragonradar/navi/DragonRouteActivity.java
+++ app/src/main/java/rw/qtopie/dragonradar/navi/DragonRouteActivity.java
@@ -1,11 +1,19 @@
 package rw.qtopie.dragonradar.navi;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.amap.api.navi.AmapRouteActivity;
+
+import rw.qtopie.dragonradar.MainActivity;
 
 public class DragonRouteActivity extends AmapRouteActivity {
     public static final String DEBUG_TAG = "dragon-radar-swipe";
@@ -20,15 +28,23 @@ public class DragonRouteActivity extends AmapRouteActivity {
         Context context = this.getApplicationContext();
         bikeIntent = new Intent(context, HelloBikeNaviActivity.class);
         naviService = new Intent(context, DragonRadarNaviService.class);
+        if (Build.VERSION.SDK_INT >= 33) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 101);
+            } else {
+                startForegroundService(naviService);
+            }
+        }
+
         naviStarted = true;
     }
+
 
     @Override
     public void onBackPressed() {
         Log.d(DEBUG_TAG, "back button pressed");
 
         if (naviStarted) {
-            startForegroundService(naviService);
             startActivity(bikeIntent);
         }
     }
